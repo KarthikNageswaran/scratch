bbbeabe328d4575fd9b6f1c67728872a76cf78cb
Nathan Lane <nathamberlane@gmail.com>
2013-08-28 14:32:37 -0600

    Updating algorithms to use pointers and memory allocation.

--- cs2430/sorting/sorting.c
+++ cs2430/sorting/sorting.c
@@ -7,13 +7,14 @@
  */
 
 #include "bubblesort.h"
+#include "randomgenerator.h"
 #include "printarrays.h"
 
 #define MAX_NUM_ELEMENTS 10
 
 int main()
 {
-	int intList[MAX_NUM_ELEMENTS] = { 2, 5, 1, 3, 7, 9, 4, 8, 6, 10 };
+	int * intList = generateRandomArray(MAX_NUM_ELEMENTS);
 
   printarray(intList, MAX_NUM_ELEMENTS);
 
