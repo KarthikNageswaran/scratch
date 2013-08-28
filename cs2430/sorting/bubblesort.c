b062fc53006850f424b6fc6dfda155c273688d27
Nathan Lane <nathamberlane@gmail.com>
2013-08-28 08:33:25 -0600

    Changing to algorithm I thought of last night for bubble sort.

--- cs2430/sorting/bubblesort.c
+++ cs2430/sorting/bubblesort.c
@@ -12,45 +12,26 @@
 
 void bubblesort(int unsortedList[], int numberOfElements)
 {
-	bool listIsSorted = false;
-	int index = 0;
-	int checkIndex = 0;
-	int swapValue = 0;
+  bool itemsWereSwapped;
+  int index;
+  int swapValue;
 
-	while (true) {
-		if (index > (numberOfElements - 2))
-		{
-			index = 0;
-		}
+  do
+  {
+    itemsWereSwapped = false;
 
-		if (unsortedList[index] > unsortedList[(index + 1)])
-		{
-			swapValue = unsortedList[index];
+    for (index = 0; index < (numberOfElements - 1); index++)
+    {
+      if (unsortedList[index] > unsortedList[(index + 1)])
+      {
+        swapValue = unsortedList[index];
 
-			unsortedList[index] = unsortedList[(index + 1)];
-			unsortedList[(index + 1)] = swapValue;
-		}
+        unsortedList[index] = unsortedList[(index + 1)];
+        unsortedList[(index + 1)] = swapValue;
 
-		index++;
-
-		if (index == (numberOfElements - 1))
-		{
-			listIsSorted = true;
-
-			for (checkIndex = 0; checkIndex < (numberOfElements - 1); checkIndex++)
-			{
-				if (unsortedList[checkIndex] > unsortedList[(checkIndex + 1)])
-				{
-					listIsSorted = false;
-
-					break;
-				}
-			}
-		}
-
-		if (listIsSorted)
-		{
-			break;
-		}
-	}
+        itemsWereSwapped = true;
+      }
+    }
+  }
+  while (itemsWereSwapped);
 }
