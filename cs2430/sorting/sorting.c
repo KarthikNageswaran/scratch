e1272b19be6cd58cea4487629fc7db412facba3f
Nathan Lane <nathamberlane@gmail.com>
2013-08-30 16:38:47 -0600

    Moving functions out into headers.

--- cs2430/sorting/sorting.c
+++ cs2430/sorting/sorting.c
@@ -15,95 +15,9 @@
 #include "quicksort.h"
 #include "arbslowsort.h"
 #include "randomgenerator.h"
-#include "printarrays.h"
-#include "lists.h"
+#include "sortingfunctioncalls.h"
 
 #define MAX_NUM_ELEMENTS 10
-
-/**
- * Note: using clock() and CLOCKS_PER_SEC came from:
- *   URL: http://stackoverflow.com/a/5249150
- *   Accessed on: 28 August 2013
- */
-double sortUsingArbitrarilySlowSort(int * unsortedList, const size_t size)
-{
-  int * copyOfList;
-  clock_t startOfTask;
-  clock_t endOfTask;
-  
-  copyOfList = makeDeepCopyOfArray(unsortedList, size);
-  
-  printf("Unsorted list (ArbSlow Sort): ");
-  printarray(copyOfList, size);
-
-  startOfTask = clock();
-  
-  arbslowsort(copyOfList, size);
-  
-  endOfTask = clock();
-
-  printf("Sorted list (ArbSlow Sort): ");
-  printarray(copyOfList, size);
-  
-  return (double)((endOfTask - startOfTask) / CLOCKS_PER_SEC);
-}
-
-/**
- * Note: using clock() and CLOCKS_PER_SEC came from:
- *   URL: http://stackoverflow.com/a/5249150
- *   Accessed on: 28 August 2013
- */
-double sortUsingBubbleSort(int * unsortedList, const size_t size)
-{
-  int * copyOfList;
-  clock_t startOfTask;
-  clock_t endOfTask;
-  
-  copyOfList = makeDeepCopyOfArray(unsortedList, size);
-  
-  printf("Unsorted list (Bubble Sort): ");
-  printarray(copyOfList, size);
-
-  startOfTask = clock();
-  
-  bubblesort(copyOfList, size);
-  
-  endOfTask = clock();
-
-  printf("Sorted list (Bubble Sort): ");
-  printarray(copyOfList, size);
-  
-  return (double)((endOfTask - startOfTask) / CLOCKS_PER_SEC);
-}
-
-/**
- * Note: using clock() and CLOCKS_PER_SEC came from:
- *   URL: http://stackoverflow.com/a/5249150
- *   Accessed on: 28 August 2013
- */
-
-double sortUsingQuicksort(int * unsortedList, const size_t size)
-{
-  int * copyOfList;
-  clock_t startOfTask;
-  clock_t endOfTask;
-  
-  copyOfList = makeDeepCopyOfArray(unsortedList, size);
-  
-  printf("Unsorted list (Quicksort): ");
-  printarray(copyOfList, size);
-
-  startOfTask = clock();
-  
-  quicksort(copyOfList, size);
-  
-  endOfTask = clock();
-
-  printf("Sorted list (Quicksort): ");
-  printarray(copyOfList, size);
-  
-  return (double)((endOfTask - startOfTask) / CLOCKS_PER_SEC);
-}
 
 int main()
 {
---
+++ cs2430/sorting/sortingfunctioncalls.h
@@ -0,0 +1,20 @@
+/*
+ * sortingfunctioncalls.h
+ *
+ *  Created on: Aug 30, 2013
+ *      Author: Nathan Lane
+ *       Class: CSIS-2430
+ */
+
+#ifndef SORTINGFUNCTIONCALLS_H_
+#define SORTINGFUNCTIONCALLS_H_
+
+#include <stdlib.h>
+
+double sortUsingArbitrarilySlowSort(int * unsortedList, const size_t size);
+
+double sortUsingBubbleSort(int * unsortedList, const size_t size);
+
+double sortUsingQuicksort(int * unsortedList, const size_t size);
+
+#endif /* SORTINGFUNCTIONCALLS_H_ */
