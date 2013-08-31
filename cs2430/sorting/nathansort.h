ad04bea0a3452587f575a13ae2e26092c1b526fb
nathandelane <nathamberlane@gmail.com>
2013-08-31 09:18:20 -0600

    Update and rename arbslowsort.h to nathansort.h

---
+++ cs2430/sorting/nathansort.h
@@ -0,0 +1,16 @@
+/*
+ * nathan.h
+ *
+ *  Created on: Aug 30, 2013
+ *      Author: Nathan Lane
+ *       Class: CSIS-2430
+ */
+
+#ifndef NATHANSORT_H_
+#define NATHANSORT_H_
+
+#include <stdlib.h>
+
+void nathansort(int *, const size_t);
+
+#endif /* NATHANSORT_H_ */
