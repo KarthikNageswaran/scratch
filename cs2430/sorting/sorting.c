f001fea9d3de2b12477ed8b8216b3da457844003
Nathan Lane <nathamberlane@gmail.com>
2013-08-30 19:35:39 -0600

    Fixing formatting in sorting.

--- cs2430/sorting/sorting.c
+++ cs2430/sorting/sorting.c
@@ -52,10 +52,10 @@ int main(int argc, char * argv[])
   /* Using ' to format numbers with commas between every third and fourth digit.
    * URL: http://stackoverflow.com/questions/11694901/how-can-i-format-currency-with-commas-in-c; Accessed on 29 August
    * 2013 */
-  printf("Clocks per second: %'li.\n\n", CLOCKS_PER_SEC);
-  printf("Time for bubble sort: %'.4lf sec\n\n", sortUsingBubbleSort(intList, numElements));
-  printf("Time for quicksort: %'.4lf sec\n\n", sortUsingQuicksort(intList, numElements));
-  printf("Time for arbslowsort: %'.4lf sec\n\n", sortUsingArbitrarilySlowSort(intList, numElements));
+  printf("Clocks per second: %li.\n\n", CLOCKS_PER_SEC);
+  printf("Time for bubble sort: %lf sec\n\n", sortUsingBubbleSort(intList, numElements));
+  printf("Time for quicksort: %lf sec\n\n", sortUsingQuicksort(intList, numElements));
+  printf("Time for arbslowsort: %lf sec\n\n", sortUsingArbitrarilySlowSort(intList, numElements));
 
   return 0;
 }
