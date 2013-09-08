b019c4cae7b6e2c419f05ea8be32210f3aea5092
Nathan Lane <nathamberlane@gmail.com>
2013-09-07 23:58:57 -0600

    Adding build scripts.

---
+++ cs2430/modularexpansion/build_ch4_assignments.bat
@@ -0,0 +1,19 @@
+@echo off
+
+REM Script to compile assignments for chapter 4.
+REM
+REM  Created on: Sep 7, 2013
+REM      Author: Nathan Lane
+REM       Class: CSIS-2430
+
+echo Building chapter 4 problem 25 solution.
+gcc -Wall -g3 -O0 -o chapter4_25 lists.h lists.c algorithmone.h algorithmone.c algorithmfive.h algorithmfive.c chapter4_25.c
+
+echo Building chapter 4 problem 26 solution.
+gcc -Wall -g3 -O0 -o chapter4_26 lists.h lists.c algorithmone.h algorithmone.c algorithmfive.h algorithmfive.c chapter4_26.c
+
+echo Building chapter 4 problem 27 solution.
+gcc -Wall -g3 -O0 -o chapter4_27 lists.h lists.c algorithmone.h algorithmone.c algorithmfive.h algorithmfive.c chapter4_27.c
+
+echo Building chapter 4 problem 28 solution.
+gcc -Wall -g3 -O0 -o chapter4_28 lists.h lists.c algorithmone.h algorithmone.c algorithmfive.h algorithmfive.c chapter4_28.c
