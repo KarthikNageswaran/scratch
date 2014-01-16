ce8c6fdc924a49c9c2266f92082b07e1645edc69
Nathan Lane <nathamberlane@gmail.com>
2014-01-16 10:26:25 -0700

    Adding asignment description.

---
+++ cs2440/lab1/index.php
@@ -0,0 +1,28 @@
+/**
+ * Author: Nathan Lane
+ * Class: cs2440-002 Spring 2014
+ * Description:
+ *   PHP and MySQL Lab1
+ *
+ *   Address book and contact page
+ *
+ *   Tasks to Complete:
+ *   - Create a form page with input for:
+ *     - First name
+ *     - Last name
+ *     - Phone
+ *     - Address
+ *     - City
+ *     - State (should be a drop-down list)
+ *     - ZIP code
+ *   - Create a button for update/create/search
+ *     - These buttons will lead the form to a 
+ *       response page (named what you wish)
+ *   - Create a response page that will:
+ *     - Display the search results
+ *     - Update a record
+ *     - Create a new record
+ *
+ *  For an example go to http://csis02.slcc.edu/dhunter3/Index.htm
+ */
+
