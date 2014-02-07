315383b622b6b2e6f3812d69f2ce30771302085c
Nathan Lane <nathamberlane@gmail.com>
2014-02-06 21:24:43 -0700

    Adding changes from server.

--- cs2440/lab1/web/_index.inc.php
+++ cs2440/lab1/web/_index.inc.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * _index.php
+ * _index.inc.php
  *
  * This is the code-behind for the index page.
  *
@@ -19,15 +19,25 @@ define('ADDRESS_LINE2_KEY', 'addressLine2');
 define('CITY_KEY', 'city');
 define('STATE_KEY', 'state');
 define('ZIP_KEY', 'zip');
-define('SUBMIT_BUTTON_KEY', 'actionButton');
-define('CREATE_BUTTON_VALUE', 'createContact');
-define('UPDATE_BUTTON_VALUE', 'updateContact');
-define('SEARCH_BUTTON_VALUE', 'searchContacts');
+define('SUBMIT_BUTTON_KEY', 'submitButton');
+define('CREATE_BUTTON_VALUE', 'Create Contact');
+define('UPDATE_BUTTON_VALUE', 'Update Contact');
+define('SEARCH_BUTTON_VALUE', 'Search Contacts');
 
 $states = queryStates();
 
+function createNewUser($firstName, $lastName, $phone, $addressLine1, $addressLine2, $city, $state, $zip) {
+  addContact($firstName, $lastName, $phone, $addressLine1, $addressLine2, $city, $state, $zip);
+}
+
+function queryUser($firstName, $lastName, $phone, $addressLine1, $addressLine2, $city, $state, $zip) {
+}
+
+function updateUser($firstName, $lastName, $phone, $addressLine1, $addressLine2, $city, $state, $zip) {
+}
+
 function processPostBack() {
-  if (defined($_POST[SUBMIT_BUTTON_KEY])) {
+  if (isset($_POST[SUBMIT_BUTTON_KEY])) {
     $firstName = $_POST[FIRST_NAME_KEY];
     $lastName = $_POST[LAST_NAME_KEY];
     $phone = $_POST[PHONE_KEY];
@@ -49,16 +59,7 @@ function processPostBack() {
   }
 }
 
-function createNewUser($firstName, $lastName, $phone, $addressLine1, $addressLine2, $city, $state, $zip) {
-}
-
-function queryUser($firstName, $lastName, $phone, $addressLine1, $addressLine2, $city, $state, $zip) {
-}
-
-function updateUser($firstName, $lastName, $phone, $addressLine1, $addressLine2, $city, $state, $zip) {
-}
-
-if (isset($_SERVER['REQUEST_METHOD']) && strcmp(strtoupper($_SERVER['REQUEST_METHOD']), 'POST') == 0) {
+if (isset($_POST)) {
   processPostBack();
 }
 
--- cs2440/lab1/web/index.php
+++ cs2440/lab1/web/index.php
@@ -17,8 +17,8 @@
  *     - City
  *     - State (should be a drop-down list)
  *     - ZIP code
- *   - Create a button for update/create/search
- *     - These buttons will lead the form to a 
+ *   - Create a submit for update/create/search
+ *     - These submits will lead the form to a 
  *       response page (named what you wish)
  *   - Create a response page that will:
  *     - Display the search results
@@ -28,7 +28,7 @@
  *  For an example go to http://csis02.slcc.edu/dhunter3/Index.htm
  */
 
-require (dirname(__FILE__) . '/_index.inc.php');
+require_once (dirname(__FILE__) . '/_index.inc.php');
 
 ?>
 <!DOCTYPE html>
@@ -37,59 +37,90 @@ require (dirname(__FILE__) . '/_index.inc.php');
     <meta content="text/html; charset=utf-8" http-equiv="content-type"/>
     <title>Contact Book | Nathan Lane CSIS-2440-002-Sp14</title>
     <link rel="stylesheet" href="css/contact-book.css" />
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
     <script type="text/javascript" src="js/contact-book.js"></script>
   </head>
   <body>
     <div id="root">
-      <form id="contactForm" name="contactForm" method="post" action="index.php">
-        <fieldset>
-          <span class="validationMessage">These fields are required to create a new contact.</span>
-        </fieldset>
-        <fieldset>
-          <input name="firstName" id="firstName" maxlength="30" />
-          <input name="lastName" id="lastName" maxlength="30" />
-        </fieldset>
-        <fieldset>
-          <input name="phone" id="phone" maxlength="10" />
-        </fieldset>
-        <fieldset>
-          <input name="addressLine1" id="addressLine1" maxlength="32" />
-          <input name="addressLine2" id="addressLine2" maxlength="32" />
-          <input name="city" id="city" maxlength="45" />
-          <select id="state" name="state">
+      <div id="formContainer">
+        <h1>Contact Book</h1>
+        <form id="contactForm" name="contactForm" method="post" action="index.php">
+          <fieldset>
+            <span class="validationMessage">These fields are required to create a new contact.</span>
+          </fieldset>
+          <fieldset>
+            <label class="requiredField" for="firstName">First Name:</label>
+            <input name="firstName" id="firstName" maxlength="30" />
+            <label class="requiredField" for="lastName">Last Name:</label>
+            <input name="lastName" id="lastName" maxlength="30" />
+          </fieldset>
+          <fieldset>
+            <label class="requiredField" for="phone">Phone Number:</label>
+            <input name="phone" id="phone" maxlength="10" />
+          </fieldset>
+          <fieldset>
+            <label class="requiredField" for="addressLine1">Address Line 1:</label>
+            <input name="addressLine1" id="addressLine1" maxlength="32" />
+            <label for="addressLine2">Address Line 2:</label>
+            <input name="addressLine2" id="addressLine2" maxlength="32" />
+            <label class="requiredField" for="city">City:</label>
+            <input name="city" id="city" maxlength="45" />
+            <label class="requiredField" for="state">State:</label>
+            <select id="state" name="state">
 <?php
 
 foreach ($states as $stateName => $stateCode) {
 
 ?>
-            <option value="<?php echo "$stateCode"; ?>"><?php echo "$stateName"; ?></option>
+              <option value="<?php echo "$stateCode"; ?>"><?php echo "$stateCode - $stateName"; ?></option>
 <?php
 
 }
 
 ?>
-          </select>
-          <input name="zip" id="zip" maxlength="10" />
-        </fieldset>
-      </form>
-    </div>
-    <div id="noJavascriptCovering">
-      <div class="message">
-        <h1>This Site Requires JavaScript to be Enabled</h1>
-        <p>
-          This website uses modern web technologies, including CSS3, and JavaScript, which are required 
-          to make it perform and operate as expected.
-        </p>
-        <p>
-          Please enable JavaScript on your browser.
-        </p>
-        <p>
-          If you are unsure how to enable JavaScript on your browser please consult your browser vendor's documentation - usually there is a website for your browser, or google "how to turn on JavaSCript in <browser-name>."
-        </p>
-        <p>
-          Thank you.
-        </p>
+            </select>
+            <label class="requiredField" for="zip">ZIP Code:</label>
+            <input name="zip" id="zip" maxlength="10" />
+          </fieldset>
+          <fieldset>
+            <input type="button" name="submitButton" id="createButton" value="Create Contact"/>
+            <input type="button" name="submitButton" id="searchButton" value="Search Contacts"/>
+            <input type="button" name="submitButton" id="updateButton" value="Update Contact"/>
+          </fieldset>
+        </form>
+      </div>
+      <div id="resultsContainer">
+        <table id="searchResultsTable">
+          <thead>
+            <tr>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>City</th>
+              <th>ZIP Code</th>
+            </tr>
+          </thead>
+        </table>
       </div>
     </div>
+    <noscript>
+      <div id="noJavascriptCovering">
+        <div class="message">
+          <h1>This Site Requires JavaScript to be Enabled</h1>
+          <p>
+            This website uses modern web technologies, including CSS3, and JavaScript, which are required 
+            to make it perform and operate as expected.
+          </p>
+          <p>
+            Please enable JavaScript on your browser.
+          </p>
+          <p>
+            If you are unsure how to enable JavaScript on your browser please consult your browser vendor's documentation - usually there is a website for your browser, or google "how to turn on JavaSCript in <browser-name>."
+          </p>
+          <p>
+            Thank you.
+          </p>
+        </div>
+      </div>
+    </noscript>
   </body>
 </html>
